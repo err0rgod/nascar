31cce1394e06f28018047fb5f0d5c8dacb3feb0b
err0rgod <ndgaming458@gmail.com>
2025-06-25 23:16:03 +0530

    ok done

--- start.py
+++ start.py
@@ -1,28 +1,8 @@
-"""
+
+
 import scapy.all as scapy
 
 ip = "192.168.75.1"
-
-arp = scapy.ARP(pdst=ip)
-broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-arp_request = broadcast / arp
-
-result = scapy.srp(arp_request, timeout=1, verbose=False)[0]
-
-if result:
-    for sent, received in result:
-        print(f"IP: {received.psrc}, MAC: {received.hwsrc}")
-else:
-    print("No devices found in the network.")
-
-
-
-"""
-
-
-import scapy.all as scapy
-
-ip = "192.168.1.1"
 
 arp=scapy.ARP(pdst=ip)
 broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
@@ -35,3 +15,4 @@ if result:
     print(f"IP: {result[0][1].psrc}, MAC: {result[0][1].hwsrc}")
 else:
     print(f"No response {ip}")
+
