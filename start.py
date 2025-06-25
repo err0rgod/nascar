06e31c50ea1fb6bd1419b8cd92622c0907f581fe
err0rgod <ndgaming458@gmail.com>
2025-06-25 23:26:32 +0530

    done baasic

--- start.py
+++ start.py
@@ -17,17 +17,3 @@ for ip in net.hosts():
         print(f"No response {ip}")
 
 
-ip = "192.168.75.1"
-
-arp=scapy.ARP(pdst=ip)
-broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-
-packet=broadcast/arp
-
-result = scapy.srp(packet,timeout=1,verbose=True)[0]
-
-if result:
-    print(f"IP: {result[0][1].psrc}, MAC: {result[0][1].hwsrc}")
-else:
-    print(f"No response {ip}")
-
