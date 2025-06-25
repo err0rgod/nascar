62914794ada64ea4ef83251ea7432db6dc4d2bc6
err0rgod <ndgaming458@gmail.com>
2025-06-25 23:26:19 +0530

    aaadded in the loop

--- start.py
+++ start.py
@@ -7,6 +7,14 @@ network = input("Enter the network in cidr format: ")
 net = ipaddress.ip_network(network, strict=False)
 for ip in net.hosts():
     print(ip)
+    arp = scapy.ARP(pdst=str(ip))
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = broadcast / arp
+    result = scapy.srp(packet, timeout=1, verbose=False)[0]
+    if result:
+        print(f"IP: {result[0][1].psrc}, MAC: {result[0][1].hwsrc}")
+    else:
+        print(f"No response {ip}")
 
 
 ip = "192.168.75.1"
