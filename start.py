cdc3cfb4ae06e71594b1454073b7e2ab414988d7
err0rgod <ndgaming458@gmail.com>
2025-06-29 23:56:07 +0530

    aded lateral movement option

--- start.py
+++ start.py
@@ -13,6 +13,7 @@ parser.add_argument("-n", "--network", type=str, help="Network in CIDR format (e
 parser.add_argument("-t", "--threads", type=int, default=10, help="Number of threads to use")
 parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
 parser.add_argument("-s", "--silent", action="store_true", help="Run in silent mode")
+parser.add_argument("-lm", "--lateral", action="store_true", help="Enable lateral movement (port scan on alive hosts)")
 args = parser.parse_args()
 
 network = args.network
@@ -50,12 +51,33 @@ def worker():
                 "mac": result[0][1].hwsrc,
                 "hostname": hostname
             }
+            # Lateral movement: port scan if enabled
+            if args.lateral:
+                info["open_ports"] = port_scan(info["ip"], COMMON_PORTS)
             with results_lock:
                 results.append(info)
-            print(f"IP: {info['ip']}, MAC: {info['mac']}, Hostname: {info['hostname']}")
+            print(f"IP: {info['ip']}, MAC: {info['mac']}, Hostname: {info['hostname']}" +
+                  (f", Open Ports: {info['open_ports']}" if args.lateral else ""))
         else:
             print(f"No response {ip}")
         ip_queue.task_done()
+
+# Define ports to scan for lateral movement
+COMMON_PORTS = [22, 80, 443, 3389, 445, 139, 21, 23, 25, 53]
+
+def port_scan(ip, ports):
+    open_ports = []
+    for port in ports:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.5)
+            result = s.connect_ex((ip, port))
+            if result == 0:
+                open_ports.append(port)
+            s.close()
+        except Exception:
+            pass
+    return open_ports
 
 # Fill the queue with all IPs
 for ip in net.hosts():
@@ -75,9 +97,18 @@ for t in threads:
 
 # Show all alive hosts at the end
 print("\nScan complete. Alive hosts:")
-print("{:<16} {:<18} {}".format("IP Address", "MAC Address", "Hostname"))
-print("-" * 50)
-for info in results:
-    print("{:<16} {:<18} {}".format(info['ip'], info['mac'], info['hostname']))
+if args.lateral:
+    print("{:<16} {:<18} {:<20} {}".format("IP Address", "MAC Address", "Hostname", "Open Ports"))
+    print("-" * 70)
+    for info in results:
+        print("{:<16} {:<18} {:<20} {}".format(
+            info['ip'], info['mac'], info['hostname'], 
+            ",".join(str(p) for p in info.get('open_ports', []))
+        ))
+else:
+    print("{:<16} {:<18} {}".format("IP Address", "MAC Address", "Hostname"))
+    print("-" * 50)
+    for info in results:
+        print("{:<16} {:<18} {}".format(info['ip'], info['mac'], info['hostname']))
 
 
