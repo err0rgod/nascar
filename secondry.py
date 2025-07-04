609c6f5d43160eef2e34ce858d6ec00d8a053884
err0rgod <ndgaming458@gmail.com>
2025-07-04 22:54:16 +0530

    verbopsity

--- secondry.py
+++ secondry.py
@@ -13,3 +13,4 @@ parser =  argparse.ArgumentParser(description="Network Scanner")
 
 parser.add_argument("-n","-network", type=str, required=True , help="Network In CIDR Form" ) # user se ip ka input lene ke liye cidr form me
 parser.add_argument("-t","-threads", type=int, default=20, help="to set the threads") # for setiing threads
+parser.add_argument("-v", "-verbose", action="store_true", help="enable verbosity") # help wale se padh na
