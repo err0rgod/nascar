70f0e55289b5c07da5561236594098bffc8732af
err0rgod <ndgaming458@gmail.com>
2025-07-04 22:51:54 +0530

    addng parsers initilaixe

---
+++ secondry.py
@@ -0,0 +1,15 @@
+import scapy.all as scapy
+import ipaddress
+import socket
+import threading
+import argparse
+from queue import Queue
+
+#imported required librarires
+
+# adding parser vars to get the user input of the required feilds
+
+parser =  argparse.ArgumentParser(description="Network Scanner")
+
+parser.add_argument("-n","-network", type=str, required=True , help="Network In CIDR Form" ) # user se ip ka input lene ke liye cidr form me
+parser.add_argument("-t","-threads", type=int, default=20, help="to set the threads") #
