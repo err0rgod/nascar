fa354a7653ec31aec86cb46b348f7a53aa77fa37
err0rgod <ndgaming458@gmail.com>
2025-07-07 22:07:37 +0530

    yayy

--- secondry.py
+++ secondry.py
@@ -67,4 +67,10 @@ def os_guess(ttl):
 #check for the ports 
 
 def port_scan(ip,ports):
-    
+    open_ports = []
+    for port in ports:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.5)
+            results = s.connect_ex((str(ip), port))
+            
