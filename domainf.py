274a4c4c2b64fd0928d7683da3f38d283324b8ce
err0rgod <ndgaming458@gmail.com>
2025-07-08 12:31:41 +0530

    wow

--- domainf.py
+++ domainf.py
@@ -5,8 +5,9 @@ df = pd.read_csv("E:\\Hunter\\Oppo\\Reconstuff\\nearmecomcn.csv", on_bad_lines='
 common = df['Common Name'].dropna().tolist()
 match = df['Matching Identities'].dropna().tolist()
 
-all_domains = common + [d for line in match for d in line.split('\n')]
-unique = sorted(set(d for d in all_domains if 'nearme.com.cn' in d.lower()))
+all_domains = common + [d for line in match for d in str(line).split('\n')]
+# Filter for domains that contain 'com.cn' (case-insensitive)
+unique = sorted(set(d for d in all_domains if 'com.cn' in d.lower()))
 
 with open("unique_subdomains.txt", "w") as f:
     f.write('\n'.join(unique))
