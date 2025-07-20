1e9bb5c3100fa648f6c61f775f2816e27151ed91
err0rgod <ndgaming458@gmail.com>
2025-07-21 00:45:09 +0530

    i don't think so

--- master.py
+++ master.py
@@ -17,14 +17,15 @@ target= args.network
 open_ports = []
 
 
+#for ports decode
+def port_ressolve(port):
+    try:
+        start_port, end_port = map(int, args.portse.split('-'))
+        ports = range(start_port, end_port + 1)
 
-try:
-    start_port, end_port = map(int, args.portse.split('-'))
-    ports = range(start_port, end_port + 1)
-
-except ValueError:
-    print("Error : Invaluid Port format. Use start - end (eg. -> 1-100)")
-    exit(1)
+    except ValueError:
+        print("Error : Invaluid Port format. Use start - end (eg. -> 1-100)")
+        exit(1)
 
 
 
