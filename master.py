8be389d8bd07dca2acac9b5247f33ba83a4c417b
err0rgod <ndgaming458@gmail.com>
2025-07-21 00:42:52 +0530

    on and on

--- master.py
+++ master.py
@@ -3,11 +3,29 @@ import argparse
 
 
 parser =  argparse.ArgumentParser(description="Gaand Phaad Network Scanner")
+
+
 parser.add_argument("-i","--network", type=str, required=True, help="Enter the target Domain or IP")
+parser.add_argument("-p","--port",type=str, default="1-1024",help="Enter the start and end of ports to scan")
+
 
 args = parser.parse_args()
 
 target= args.network
+
+
+open_ports = []
+
+
+
+try:
+    start_port, end_port = map(int, args.portse.split('-'))
+    ports = range(start_port, end_port + 1)
+
+except ValueError:
+    print("Error : Invaluid Port format. Use start - end (eg. -> 1-100)")
+    exit(1)
+
 
 
 def resolve_target(target)
