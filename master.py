deff3d00197974350d14030f9762860697c7f960
err0rgod <ndgaming458@gmail.com>
2025-07-21 10:19:58 +0530

    basic done

--- master.py
+++ master.py
@@ -18,32 +18,54 @@ port = args.port
 open_ports = []
 
 
-ports = None
+
 
 #for ports decode
-
-try:
-    start_port, end_port = map(int, args.port.split('-'))
-    ports = range(start_port, end_port)
-
-except ValueError:
-    print("Error : Invalid Port format. Use start - end (eg. -> 1-100)")
-    exit(1)
-
-
-print(ports)
+def ip_ressolve(target):
+    try:
+        start_port, end_port = map(int, args.port.split('-'))
+        ports = range(start_port, end_port)
+        return ports
+    
+    except ValueError:
+        print("Error : Invalid Port format. Use start - end (eg. -> 1-100)")
+        exit(1)
 
 
 
 
+# ressolve initial domain
 
 def resolve_target(target):
 
     ip = socket.gethostbyname(target)
     
     print(ip)
+    return ip
+
+
+#port scan without proxy
+def port_scan(ip,ports):
+    for port in ports:
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.settimeout(1)
+        if s.connect_ex((ip,port))== 0:
+            print(f"The Port {port} is Open")
+            
+        else:
+            print("port  Not open")
+
+        s.close()
+
+        
 
 
 
 
-resolve_target(target)
+
+
+
+portse = ip_ressolve(target)
+ip=resolve_target(target)
+port_scan(ip,portse)
+print(portse)
